8f7b3b997431d9bb95f4d134fdfdd76f5c10c655
huntergud <111236229+huntergud@users.noreply.github.com>
2022-08-15 00:30:21 +0900

    Update bitcoinAutotTrade.py

--- bitcoinAutotTrade.py
+++ bitcoinAutotTrade.py
@@ -52,6 +52,7 @@ while True:
         # 9:00 < 현재 < 8:59:50
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-XRP", 0.5)
+            ma15 = get_ma15("KRW-XRP")
             current_price = get_current_price("KRW-XRP")
             if target_price < current_price and ma15 < current_price:
                 krw = get_balance("KRW")
